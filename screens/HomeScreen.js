aeeaf4c0992edfaa634736de300c09a2f45a0939
Shannon M. Denna <sdenna@d211.org>
2023-03-17 08:03:49 -0500

    Added a drop down selection element for practice
     npm install react-native-dropdown-select-list

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,17 +4,35 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useAuthentication } from '../utils/hooks/useAuthentication';
 import { Button } from 'react-native-elements';
 import { getAuth, signOut } from 'firebase/auth';
+import { SelectList } from 'react-native-dropdown-select-list'
 
 const auth = getAuth();
 
 export default function HomeScreen() {
   const { user } = useAuthentication();
+  const [selected, setSelected] = React.useState("");
+
+  const data = [
+    {key:'1', value:'Mobiles', disabled:true},
+    {key:'2', value:'Appliances'},
+    {key:'3', value:'Cameras'},
+    {key:'4', value:'Computers', disabled:true},
+    {key:'5', value:'Vegetables'},
+    {key:'6', value:'Diary Products'},
+    {key:'7', value:'Drinks'},
+]
 
   return (
     <View style={styles.container}>
         <Text>Welcome {user?.email}!</Text>
 
         <Button title="Sign Out" style={styles.button} onPress={() => signOut(auth)} />
+        <SelectList 
+          setSelected={(val) => setSelected(val)} 
+          data={data} 
+          save="value"
+    />
+      
       <StatusBar style="auto" />
     </View>
   );
